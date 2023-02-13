5160943eb753eba19e03601bfa55b6f3ec92620d
susshka <milkfoxy@gmail.com>
2023-02-13 12:45:01 +0300

    Кнопка входа

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/UI/header/Header";
 import Navbar from "./components/UI/navbar/Navbar";
 import A from "./components/UI/a/A";
 import "./styles/App.css";
+import Button from "./components/UI/button/Button";
 
 function App() {
 
@@ -11,16 +12,16 @@ function App() {
   return (
     <div className="App">
       <Header>
-        <div className="title">
-        Сбор данных из различных сервисов АО "НПП "Звезда"
+        <div className="title" style={{ marginLeft:"15px"}}>
+          Сбор данных из различных сервисов АО "НПП "Звезда"
         </div>
+          <Button style={{ marginRight:"15px"}}>Войти</Button>
       </Header>
       <Navbar style={{display:"block", alignItems:"center", justifyContent:"space-evenly"}}>
         <div className="links">
-          <A href="/">Home</A>
-          
-          <A href="/info">Soft Info</A>
-          <A href="/authorization">Log in</A>
+          <A href="/">Состояние ПО</A>
+          <A href="/info">Log-файлы</A>
+          <A href="/authorization">Вход</A>
         </div>
       </Navbar>
       <Workspace/>
